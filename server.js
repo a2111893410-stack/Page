fcfa6e0c591e7e5acf3eae8acd818058b875d405
Mlml <a2111893410@gmail.com>
2026-04-19 22:28:39 +0800

    更新 server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-后端自动回复版
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
@@ -58,7 +57,7 @@ wss.on("connection", (ws) => {
                     const autoReplyPacket = {
                         from: "admin",
                         to: packet.from,
-                        text: "看片群20元永久看，上万部资源，小学初中高中，萝莉御姐熟女等等所有类型都有，每天更新，要进群吗？",
+                        text: "看片群20元永久看，上万部资源，小学初中高中，萝莉御姐熟女等等所有类型都有，每天更新，要进群吗",
                         type: "text",
                         time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
                         id: 'auto_' + Date.now()
